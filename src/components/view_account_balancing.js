0d4f64e89b757ab214bcf57d0077b1395499aeb3
AhmadHatziq <e0031550@u.nus.edu>
2021-03-05 15:28:05 +0800

    Readded toggling of balance

--- src/components/view_account_balancing.js
+++ src/components/view_account_balancing.js
@@ -16,7 +16,10 @@ export default class ViewAccountBalance extends Component {
           api_key: 'nkIw7MUaN61afevVxT2eQjJTq86GH9O6oahdb3x7', 
           account_key: '8ee1f2c6-ef52-4a6e-ae4c-1dbc5b6f0924', 
           assetBalance: '', 
-          cashBalance: ''
+          cashBalance: '', 
+          showBalance: false,
+          balanceHtml: '', 
+          table_style: 'border: 1px solid black;'
         }
     }
 
@@ -49,16 +52,54 @@ export default class ViewAccountBalance extends Component {
             assetBalance: asset_balance,
             cashBalance: cash_balance
         }); 
+
+        // Toggles the boolean value
+        const doesShow = this.state.showBalance; 
+        this.setState({showBalance: !doesShow});
     }
 
     // Render method
     render () {
+
+        let balance_details = null;
+
+        if (this.state.showBalance === true) {
+            balance_details = ( 
+                <div>
+                    <center>
+                    <table style={{border : '1px solid black'}}>
+                        <tr>
+                            <th style={{border : '1px solid black'}}>Account</th>
+                            <th style={{border : '1px solid black'}}>Balance ($)</th>
+                        </tr>
+                        <tr>
+                            <td style={{border : '1px solid black'}}>Asset Balance</td>
+                            <td style={{border : '1px solid black'}}>{this.state.assetBalance}</td>
+                        </tr>
+                        <tr>
+                            <td style={{border : '1px solid black'}}>Cash Balance</td>
+                            <td style={{border : '1px solid black'}}> {this.state.cashBalance}</td>
+                        </tr>
+                        </table>
+                    </center>
+                </div>
+            );
+        }; 
+
         return ( 
             <div>
                 <center><h1>Please view your account balance below: </h1></center>
-                <button onClick = {this.getProfile}>Trigger</button>
-                <h1>Asset Balance: {this.state.assetBalance}</h1>
-                <h1>Cash Balance: {this.state.cashBalance}</h1>
+                <button onClick = {this.getProfile}>Toggle Balance</button>
+                <p>
+                    As your balance is confidential, please ensure you are in a secure environment
+                    before viewing balance ie ensure there is no one standing behind you. 
+                </p>
+
+               <center>
+                <table>
+                    {balance_details}
+                </table>
+               </center>
             </div>
         );
     }
