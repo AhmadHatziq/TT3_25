32a179b9ebd515fe7850bbc1253496685f65331c
AhmadHatziq <e0031550@u.nus.edu>
2021-03-05 14:00:15 +0800

    Add button toggling of account balance

--- src/components/view_account_balancing.js
+++ src/components/view_account_balancing.js
@@ -6,7 +6,6 @@ import axios from 'axios';
 export default class ViewAccountBalance extends Component {
 
     // Constructor: To store state variables
-    
     constructor() {
         super()
         this.state = {
@@ -16,9 +15,15 @@ export default class ViewAccountBalance extends Component {
           api_key: 'nkIw7MUaN61afevVxT2eQjJTq86GH9O6oahdb3x7', 
           account_key: '8ee1f2c6-ef52-4a6e-ae4c-1dbc5b6f0924', 
           assetBalance: '', 
-          cashBalance: ''
+          cashBalance: '', 
+          showBalance: false,
+          balanceHtml: '', 
+          table_style: 'border: 1px solid black;'
         }
       }
+
+    
+
 
     // To get user profile  
     getProfile = async () => { 
@@ -28,7 +33,6 @@ export default class ViewAccountBalance extends Component {
         let jsonObj = {
             accountKey: this.state.account_key
         }; 
-        // console.log(jsonObj); 
 
         // Send response
         const response = await fetch(this.state.api_access_point, {
@@ -40,7 +44,7 @@ export default class ViewAccountBalance extends Component {
 
         // Get response content and extract contents
         const content = await response.json();
-        console.log(content);
+        // console.log(content);
         const asset_balance = content.assetBalance; 
         const cash_balance = content.cashBalance; 
 
@@ -49,16 +53,56 @@ export default class ViewAccountBalance extends Component {
             assetBalance: asset_balance, 
             cashBalance: cash_balance
         }); 
+
+        // Toggles the boolean value
+        const doesShow = this.state.showBalance; 
+        this.setState({showBalance: !doesShow});
     }
 
     // Render method
     render () {
+
+        let balance_details = null;
+        
+        if (this.state.showBalance === true) {
+            balance_details = ( 
+                <div>
+                    <center>
+                    <table style={{border : '1px solid black'}}>
+                        <tr>
+                            <th style={{border : '1px solid black'}}>Account</th>
+                            <th style={{border : '1px solid black'}}>Balance ($)</th>
+                        </tr>
+                        <tr>
+                            <td style={{border : '1px solid black'}}>Asset Balance</td>
+                            <td style={{border : '1px solid black'}}>{this.state.assetBalance}</td>
+                        </tr>
+                        <tr>
+                            <td style={{border : '1px solid black'}}>Cash Balance</td>
+                            <td style={{border : '1px solid black'}}> {this.state.cashBalance}</td>
+                        </tr>
+                        </table>
+                    </center>
+                </div>
+            );
+        }
+
         return ( 
             <div>
                 <center><h1>Please view your account balance below: </h1></center>
-                <button onClick = {this.getProfile}>Trigger</button>
-                <h1>Asset Balance: {this.state.assetBalance}</h1>
-                <h1>Cash Balance: {this.state.cashBalance}</h1>
+                <button onClick = {this.getProfile}>Toggle Balance</button>
+                <p>
+                    As your balance is confidential, please ensure you are in a secure environment
+                    before viewing balance ie ensure there is no one standing behind you. 
+                </p>
+               
+               <center>
+                <table>
+                    {balance_details}
+                </table>
+               </center>
+               
+        
             </div>
         ); 
     }
