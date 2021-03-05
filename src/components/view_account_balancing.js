63b241390e718c8b30111b34bba585b34ee30fcb
AhmadHatziq <e0031550@u.nus.edu>
2021-03-05 12:48:41 +0800

    finish basic functionality for viewing account balancing

--- src/components/view_account_balancing.js
+++ src/components/view_account_balancing.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import axios from 'axios';
 
+// Component to view account balance. Please refer to 'View Wallet Balances' part of Documentation
+// for API details. 
 export default class ViewAccountBalance extends Component {
 
     // Constructor: To store state variables
@@ -10,23 +12,25 @@ export default class ViewAccountBalance extends Component {
         this.state = {
           username: "Group25",
           password: "om1o6OBdD9ZwjuH", 
-          api_access_point: 'https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/login', 
-          api_key: 'nkIw7MUaN61afevVxT2eQjJTq86GH9O6oahdb3x7'
+          api_access_point: 'https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/balance', 
+          api_key: 'nkIw7MUaN61afevVxT2eQjJTq86GH9O6oahdb3x7', 
+          account_key: '8ee1f2c6-ef52-4a6e-ae4c-1dbc5b6f0924', 
+          assetBalance: '', 
+          cashBalance: ''
         }
       }
 
     // To get user profile  
     getProfile = async () => { 
-        console.log('getProfile Method launched'); 
+        // console.log('getProfile Method launched'); 
         
-
+        // Create Request Body (JSON)
         let jsonObj = {
-            username: this.state.username,
-            password: this.state.password
+            accountKey: this.state.account_key
         }; 
+        // console.log(jsonObj); 
 
-        console.log(jsonObj); 
-
+        // Send response
         const response = await fetch(this.state.api_access_point, {
             method: 'POST', 
             headers: {'x-api-key': this.state.api_key},
@@ -34,9 +38,17 @@ export default class ViewAccountBalance extends Component {
             body: JSON.stringify(jsonObj)
         });
 
+        // Get response content and extract contents
         const content = await response.json();
-
         console.log(content);
+        const asset_balance = content.assetBalance; 
+        const cash_balance = content.cashBalance; 
+
+        // Update state with content objects
+        this.setState({
+            assetBalance: asset_balance, 
+            cashBalance: cash_balance
+        }); 
     }
 
     // Render method
@@ -45,6 +57,8 @@ export default class ViewAccountBalance extends Component {
             <div>
                 <center><h1>Please view your account balance below: </h1></center>
                 <button onClick = {this.getProfile}>Trigger</button>
+                <h1>Asset Balance: {this.state.assetBalance}</h1>
+                <h1>Cash Balance: {this.state.cashBalance}</h1>
             </div>
         ); 
     }
